259d96d40ad7dc27b6f01ce032ccd7f7cec31ae1
lucapedrelli <lucapedrelli@gmail.com>
2019-08-20 18:28:40 +0200

    add metrics init

--- utils/__init__.py
+++ utils/__init__.py
@@ -1,3 +1,3 @@
 from .configurations import config_MG, config_pianomidi
 from .task import load_MG, load_pianomidi,  select_indexes
-from .metrics import MSE, computeMusicAccuracy
+from .metrics import MSE, NRMSE, MAE, MAPE, computeMusicAccuracy
