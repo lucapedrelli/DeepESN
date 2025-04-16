9cff35fbf7ea8f719410f12591a0b9647ed90d00
lucapedrelli <lucapedrelli@gmail.com>
2025-04-16 18:45:03 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,17 @@
 from setuptools import setup, find_packages
 
-setup(name='DeepESN',
-      version='1.1.2',
-      packages=find_packages(),
-      package_dir='src',
-      install_requires=[
-          'scipy',
-          'numpy'
-      ],
-      description="Deep Echo State Network Python library (DeepESNpy), 2018",
-      author='Luca Pedrelli',
-      author_email='lucapedrelli@gmail.com',
-      license='BSD 3-Clause',
-      url="https://github.com/lucapedrelli/DeepESN"
+setup(
+    name='DeepESN',
+    version='1.1.2',
+    packages=find_packages(where='src'),
+    package_dir={'': 'src'},
+    install_requires=[
+        'scipy',
+        'numpy'
+    ],
+    description="Deep Echo State Network Python library (DeepESNpy), 2018",
+    author='Luca Pedrelli',
+    author_email='lucapedrelli@gmail.com',
+    license='BSD 3-Clause',
+    url="https://github.com/lucapedrelli/DeepESN"
 )
