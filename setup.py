781e43fac7ccded15b94ddfcd473a7dce7254830
lucapedrelli <lucapedrelli@gmail.com>
2026-03-16 18:24:28 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='DeepESN',
-    version='1.1.5',
+    version='1.1.6',
     packages=find_packages(where='src'),
     package_dir={'': 'src'},
     install_requires=[
